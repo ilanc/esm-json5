0cab192c0097e7e7a0b828a74520979b136a41b1
ilan <ilan@spikedata.co.za>
2022-02-23 11:27:40 +0200

    initial

---
+++ esmImports.js
@@ -0,0 +1,12 @@
+// see: https://github.com/json5/json5/issues/240
+import json5 from "json5";
+// import { parse } from "json5";
+
+let json = `{
+  // comment
+   a: 1,
+   b: 1,
+   c: 1
+}`;
+console.log("json5.parse:", JSON.stringify(json5.parse(json)));
+// console.log("parse:", JSON.stringify(parse(json))); // 'json5' is a CommonJS module??
